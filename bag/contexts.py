2b7fa4c1a59d6f526659084154238ffde0269e06
Ben Tonks <thebentonks@gmail.com>
2020-06-11 21:40:41 +0100

    added product model and size selector to template

--- bag/contexts.py
+++ bag/contexts.py
@@ -21,7 +21,7 @@ def bag_contents(request):
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_deliver_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
