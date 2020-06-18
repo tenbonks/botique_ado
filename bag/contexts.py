a18a2146dcddc13ae7c3ffa5c1386d7d1abfb2cb
Ben Tonks <thebentonks@gmail.com>
2020-06-18 19:20:25 +0100

    Added notifications CSS, shopping bag preview, and additional messages

--- bag/contexts.py
+++ bag/contexts.py
@@ -34,10 +34,10 @@ def bag_contents(request):
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_deliver_delta = settings.FREE_DELIVERY_THRESHOLD - total
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
-        free_deliver_delta = 0
+        free_delivery_delta = 0
 
     grand_total = delivery + total
     
@@ -46,7 +46,7 @@ def bag_contents(request):
      'total' : total,
      'product_count' : product_count,
      'delivery' : delivery,
-     'free_deliver_delta' : free_deliver_delta,
+     'free_delivery_delta' : free_delivery_delta,
      'free_delivery_threshold' : settings.FREE_DELIVERY_THRESHOLD,
      'grand_total' : grand_total,
 
